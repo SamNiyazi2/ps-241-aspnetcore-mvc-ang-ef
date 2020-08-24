abb8da2d2dca443593dc42570a8f9d10ede5d927
Sam Niyazi <sam2003@niyazi.com>
2020-08-23 21:46:42 -0500

    Completed M05-04

--- Startup.cs
+++ Startup.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace ps_DutchTreat
 {
@@ -10,6 +11,10 @@ namespace ps_DutchTreat
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+
+            // 08/23/2020 09:23 pm - SSN - [20200823-2113] - [002] - M05-04 - Enabling MVC 6
+            services.AddControllersWithViews();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -18,22 +23,13 @@ namespace ps_DutchTreat
 
             // 08/23/2020 12:28 am - SSN - [20200823-0028] - [001] - M02-06 - Installing Visual Studio 2019 
 
-            //if (env.IsDevelopment())
-            //{
-            //    app.UseDeveloperExceptionPage();
-            //}
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
 
-            //app.UseRouting();
+            }
 
-            //app.UseEndpoints(endpoints =>
-            //{
-            //    endpoints.MapGet("/", async context =>
-            //    {
-            //        await context.Response.WriteAsync("Hello World!");
-            //    });
-            //});
-
-
+             
             //  app.Run(async context =>
             //{
             //    await context.Response.WriteAsync("Hellow World!!!");
@@ -41,11 +37,26 @@ namespace ps_DutchTreat
 
             // 08/23/2020 11:52 am - SSN - [20200823-1152] - [001] - M02-08 - Serving your first file
             // Order matters
-            app.UseDefaultFiles();
+
+            // 08/23/2020 09:18 pm - SSN - [20200823-2113] - [001] - M05-04 - Enabling MVC 6
+
+            // app.UseDefaultFiles();   // [20200823-2113] - [001] 
+
+
+
             app.UseStaticFiles();
 
             app.UseNodeModules();
 
+            app.UseRouting(); // [20200823-2113] - [001] 
+
+            app.UseEndpoints(cfg =>   // [20200823-2113] - [001]
+            {
+                cfg.MapControllerRoute("Fallback",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index" }
+                    );
+            });
 
         }
     }
