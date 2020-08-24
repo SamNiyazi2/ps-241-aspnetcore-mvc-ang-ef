7211321fe8b290d4115a09a91b140e2fea638d92
Sam Niyazi <sam2003@niyazi.com>
2020-08-24 13:59:21 -0500

    Complete M07-02 (2)

---
+++ Data/DutchContext.cs
@@ -0,0 +1,23 @@
+﻿using DutchTreat.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 08/24/2020 01:54 pm - SSN - [20200824-1335] - [001] - M07-01 - Creating entities
+
+namespace ps_DutchTreat.Data
+{
+    public class DutchContext : DbContext
+    {
+
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Order> Orders { get; set; }
+
+
+
+    }
+}
