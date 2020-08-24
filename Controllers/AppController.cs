58fd22022d555024c4c0ec9f96e6346a09018bde
Sam Niyazi <sam2003@niyazi.com>
2020-08-23 21:11:07 -0500

    Completed M05-03

---
+++ Controllers/AppController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 08/23/2020 08:39 pm - SSN - [20200823-2037] - [001] - M05-03 - First controller/view 
+
+namespace ps_DutchTreat.Controllers
+{
+    public class AppController: Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
