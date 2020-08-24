f8be064d461e68f212500ebcb998c1b6e5f658ac
Sam Niyazi <sam2003@niyazi.com>
2020-08-24 02:42:55 -0500

    Completed M05-11

--- Controllers/AppController.cs
+++ Controllers/AppController.cs
@@ -29,7 +29,17 @@ namespace ps_DutchTreat.Controllers
 
         [HttpPost("contact")]
         public IActionResult Contact( ContactViewModel model)
-        {
+        { 
+
+
+            if (ModelState.IsValid)
+            {
+
+            }
+            else
+            {
+
+            }
             return View();
         }
 
--- ViewModels/ContactViewModel.cs
+++ ViewModels/ContactViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,10 +9,16 @@ namespace ps_DutchTreat.ViewModels
 {
     public class ContactViewModel
     {
-
+        [Required]
+        [MinLength(5, ErrorMessage= "Must be at least five characters long.")]
         public string Name { get; set; }
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+        [Required]
         public string Subject { get; set; }
+        [Required]
+        [MaxLength(250, ErrorMessage ="Cannot exceed 250 characters.")]
         public string Message { get; set; }
 
     }
