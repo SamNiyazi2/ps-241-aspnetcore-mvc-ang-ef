6a4be2279f83fa6fd533cf93b781e70a81683945
Sam Niyazi <sam2003@niyazi.com>
2020-08-24 13:08:29 -0500

    Commit process to set focus from the validation summary

--- wwwroot/js/index.js
+++ wwwroot/js/index.js
@@ -1,3 +1,39 @@
 ﻿
 // 08/23/2020 07:52 pm - SSN - [20200823-1952] - [001] - M04-05 - Using NPM 
 
+
+//$(document).ready(function () {});
+
+
+function setupValidationSummaryErrors() {
+
+    setTimeout(setupValidationSummaryErrors_setup, 2000);
+
+}
+
+function setupValidationSummaryErrors_setup() {
+
+
+
+    $(".validation-summary-errors li").bind('click', function (e) {
+        let textMessage = $(this).text();
+
+        $(".form-control").each(function (ndx, obj1) {
+            $.each(this.attributes, function (i, attribute) {
+
+                if (attribute.nodeName.startsWith('data-val-')) {
+
+                    if (textMessage === attribute.nodeValue) {
+
+                        $(this.ownerElement).focus();
+                    }
+                }
+            });
+        });
+    });
+
+    $(".validation-summary-errors li").each(function (i, obj1) {
+        $(obj1).addClass('cssLinkedErrorMessage');
+    });
+
+}
