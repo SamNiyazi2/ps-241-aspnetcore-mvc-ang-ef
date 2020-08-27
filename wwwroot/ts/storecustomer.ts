f50ee27a46e8839755c9a0e6d431c8f0e862066a
Sam Niyazi <sam2003@niyazi.com>
2020-08-27 13:33:58 -0500

    Completed M10-02

---
+++ wwwroot/ts/storecustomer.ts
@@ -0,0 +1,51 @@
+﻿
+// 08/27/2020 01:22 pm - SSN - [20200827-1321] - [001] - M10-02 - First TypeScript class
+
+
+
+class StoreCustomer {
+
+
+    //constructor(theName: string) {
+    //    this.name = theName;
+    //}
+
+    constructor(private firstName: string, private lastName: string) {
+
+    }
+
+
+    // private / protected / public
+    public visit: number = 0;
+    private ourName: string;
+
+    public showName() {
+
+        console.log('StoreCustomre - showName');
+        console.log(this.firstName + ' ' + this.lastName);
+
+        return true;
+    }
+
+    public showName_withParam(name: string) {
+
+        console.log('StoreCustomre - showName');
+        console.log(name);
+
+        return true;
+    }
+
+    set name(name: string) {
+        this.ourName = name;
+    }
+
+    get name() {
+        return this.ourName;
+    }
+
+
+}
+
+
+let cust = new StoreCustomer();
+
