f963d6d9876e7dcc1055c0a055db13afcb8e5d9a
Sam Niyazi <sam2003@niyazi.com>
2020-08-31 14:32:43 -0500

    Completed M11-05

---
+++ ClientApp/app/app.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <!--The content below is only a placeholder and can be replaced.-->
+    <div style="text-align:center" class="content">
+      <h1>
+        Welcome to {{title}}!
+      </h1>
+    </div>
+    
+    
+  `,
+  styles: []
+})
+export class AppComponent {
+  title = 'Dutch Treat';
+}
