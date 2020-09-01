ea8eae0d3c674c17002b18fe9546f007f654e8b1
Sam Niyazi <sam2003@niyazi.com>
2020-08-31 19:43:59 -0500

    Complete M11

--- ClientApp/app/app.module.ts
+++ ClientApp/app/app.module.ts
@@ -2,15 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductList } from './shop/productList.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        ProductList
+    ],
+    imports: [
+        BrowserModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ ClientApp/app/shop/productList.component.ts
@@ -0,0 +1,26 @@
+﻿// 08/31/2020 06:44 pm - SSN - [20200831-1843] - [001] - M11-08 - Your first Angular component
+
+import { Component } from '@angular/core';
+
+@Component({
+    selector: "product-list",
+    templateUrl: "./productList.component.html",
+    styleUrls:[]
+})
+export class ProductList {
+
+    public products = [
+        {
+            title: 'First product',
+            price: 19.99
+        },
+        {
+            title: 'Second product',
+            price: 9.99
+        },
+        {
+            title: 'Third product',
+            price: 14.99
+        },
+    ]
+}
