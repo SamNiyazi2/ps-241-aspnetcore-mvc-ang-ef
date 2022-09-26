7c766dd92115498615bdcd89c894c0b0ab251991
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-09-26 09:48:13 -0500

    Failed to pickup NuGet package  SSN_Angular_Script_Util. Reference remote NuGet.

---
+++ dotnet_add_package_Angular_Util_Remote.cmd
@@ -0,0 +1,6 @@
+
+: 09/07/2019 03:19 am - SSN
+: 09/16/2019 01:14 pm - SSN - Remove
+
+dotnet remove package SSN_Angular_Scripts_util 
+dotnet add package SSN_Angular_Scripts_util -s https://api.nuget.org/v3/index.json
