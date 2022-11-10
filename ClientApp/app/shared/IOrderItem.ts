d89e48071b1d2202552e32e309d83d570566c09b
Sam Niyazi <24701208+SamNiyazi2@users.noreply.github.com>
2022-11-10 01:32:02 -0600

    Update IOrderItem

--- ClientApp/app/shared/IOrderItem.ts
+++ ClientApp/app/shared/IOrderItem.ts
@@ -1,6 +1,8 @@
 
 // 09/01/2020 03:53 pm - SSN - [20200901-1547] - [002] - M12-07 - Sharing data across components
 
+import { IProduct } from './product';
+
 export interface IOrderItem {
     id: number;
     quantity: number;
@@ -11,4 +13,8 @@ export interface IOrderItem {
     productTitle: string;
     productArtist: string;
     productArtId: string;
+
+    // 11/10/2022 01:31 am - SSN - Add product;
+    product: IProduct;
+
 }
